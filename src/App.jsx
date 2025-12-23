837fd86804009ccf10416d150cc714d12a7ebcf1
alane-vaibhav <alane.vaibhav@gmail.com>
2025-12-23 11:06:47 +0530

    Added todo list code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import "./App.css";
 import Accordian from "./components/accordian";
+import ToDoApp from "./components/todolist";
 
 function App() {
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        rowGap: "5px",
+      }}
+    >
       <Accordian />
+      <ToDoApp />
     </div>
   );
 }
---
+++ src/components/todolist/index.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+const ToDoApp = () => {
+  const [item, setItem] = useState("");
+  const [list, setList] = useState([]);
+
+  const handleAddition = () => {
+    setList([...list, item]);
+    setItem("");
+  };
+
+  const handleRemove = (id) => {
+    let newList = [...list];
+    newList.splice(id, 1);
+    setList(newList);
+  };
+  return (
+    <div>
+      <h2>To do App</h2>
+
+      <div>
+        <input
+          type="text"
+          placeholder="add item..."
+          value={item}
+          onChange={(e) => setItem(e.target.value)}
+        />
+        <button onClick={handleAddition}>Add item</button>
+      </div>
+      {list.map((el, index) => (
+        <>
+          <h4>{el}</h4>
+          <button onClick={() => handleRemove(index)}>Remove item</button>
+        </>
+      ))}
+    </div>
+  );
+};
+
+export default ToDoApp;
